3b067edb8a42c74d0a20cf6cae65583a5b335f43
evans1012 <evansodundo46@gmail.com>
2023-08-11 13:06:44 +0300

    forgotpasswordpageupdate

--- src/components/forgot_password.jsx
+++ src/components/forgot_password.jsx
@@ -7,143 +7,142 @@ const ForgotPassword = () => {
   const [confirmPassword, setConfirmPassword] = useState("");
   const [showPasswordForm, setShowPasswordForm] = useState(false);
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+
+  const isEmailValid = (email) => {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
+  };
+
+  const isStrongPassword = (password) => {
+    const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
+    return passwordPattern.test(password);
+  };
 
   const submitForm = () => {
-    
-    if (email) {
+    if (isEmailValid(email)) {
       setShowPasswordForm(true);
-  
+      setEmailError("");
     } else {
-      alert("Please enter a valid email address.");
+      setEmailError("Please enter a valid email address.");
     }
   };
 
   const updatePassword = () => {
-   
-
-    if (password && password === confirmPassword) {
-    
+    if (isStrongPassword(password) && password === confirmPassword) {
       setShowPasswordForm(false);
       setShowSuccessMessage(true);
+      setPasswordError("");
     } else {
-      alert("Passwords do not match. Please try again.");
+      setPasswordError("Passwords do not match or do not meet the password requirements.");
     }
   };
 
   return (
-    <div style={styles.container}>
-      <div id="emailForm" style={{ display: !showPasswordForm && !showSuccessMessage ? "block" : "none" }}>
-        <div className="form-group">
+    <div style={styles.pageBackground}>
+      <div style={styles.container}>
+        <center>
+          <img src="./assets/SEED full logo.png" alt="Logo" />
+        </center>
+        <div id="emailForm" style={{ display: !showPasswordForm && !showSuccessMessage ? "block" : "none" }}>
+          <div className="form-group">
+            <center>
+              <h2><b>Forgot Password ?</b></h2>
+            </center>
+            <br />
+            <center>
+              <h3>Enter Email Address</h3>
+            </center>
+            <input
+              type="email"
+              id="email"
+              placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              style={styles.input}
+            />
+            <p style={{ color: 'red' }}>{emailError}</p>
+          </div>
+          <br />
           <center>
-          <h2><b>Forgot Password ?</b></h2>
+            <h4> Back to <Link to="/login"><b>Sign In</b></Link></h4>
+          </center>
+          <br />
+          <button onClick={submitForm} style={styles.button}>Send Code</button>
+          <br />
+          <br />
+          <center>
+            <h4> Don't have an account? <Link to="/sign_up"><b>Sign Up</b></Link></h4>
+          </center>
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+        </div>
+        <div id="passwordForm" style={{ display: showPasswordForm && !showSuccessMessage ? "block" : "none" }}>
+          <div className="form-group">
+            <center>
+              <h3><b>Enter Verification Code to Reset Password</b></h3>
+            </center>
+            <br />
+            <label htmlFor="code">Enter Code:</label>
+            <input
+              type="code"
+              id="code"
+              placeholder="Enter Verification Code"
+              style={styles.input}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">New Password:</label>
+            <input
+              type="password"
+              id="password"
+              placeholder="Enter your new password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              style={styles.input}
+            />
+            <p style={{ color: 'red' }}>{passwordError}</p>
+          </div>
+          <div className="form-group">
+            <label htmlFor="confirmPassword">Confirm Password:</label>
+            <input
+              type="password"
+              id="confirmPassword"
+              placeholder="Confirm your new password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              style={styles.input}
+            />
+          </div>
+          <br />
+          <center>
+            <h4>Didn't Receive Code ? <b><a href="http://localhost:3000/forgot_password">Resend</a></b></h4>
+          </center>
+          <button onClick={updatePassword} style={styles.button}>Update Password</button>
+          <br />
+          <br />
+          <center>
+            <h4> Don't have an account? <Link to="/sign_up"><b>Sign Up</b></Link></h4>
           </center>
           <br />
           <center>
-            <h3>Enter Email Address</h3>
+            <h3>OR</h3>
           </center>
-
-          <input
-            type="email"
-            id="email"
-            placeholder="Enter your email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            style={styles.input}
-          />
-        </div>
-        <br />
-        <center>
-          <h4> Back to <Link to="/login">
-                <b>Sign In</b>
-       </Link>
-</h4>
-        </center>
-        <br />
-
-        <button onClick={submitForm} style={styles.button}>Send Code</button>
-        <br />
-        <br />
-
-        <center>
-          <h4> Dont have an account ? <Link to="/sign_up">
-                <b>Sign Up</b>
-       </Link>
-</h4>
-        </center>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-      </div>
-
-      <div id="passwordForm" style={{ display: showPasswordForm && !showSuccessMessage ? "block" : "none" }}>
-        <div className="form-group">
+          <br />
           <center>
-            <h3><b>Enter Verification Code </b></h3>
+            <h4> <Link to="/login"><b>Sign In</b></Link></h4>
           </center>
-          <label htmlFor="code">Enter Code:</label>
-          <input
-            type="code"
-            id="code"
-            placeholder="Enter Verification Code"
-            style={styles.input}
-          />
-
         </div>
-        <div className="form-group">
-          <label htmlFor="password">New Password:</label>
-          <input
-            type="password"
-            id="password"
-            placeholder="Enter your new password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            style={styles.input}
-          />
+        <div id="successMessage" className="success-message" style={{ display: showSuccessMessage ? "block" : "none" }}>
+          <p>Password updated successfully! You can now Sign In with your new password.</p>
+          <h4> <Link to="/login"><b>Sign In</b></Link></h4>
         </div>
-        <div className="form-group">
-          <label htmlFor="confirmPassword">Confirm Password:</label>
-          <input
-            type="password"
-            id="confirmPassword"
-            placeholder="Confirm your new password"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            style={styles.input}
-          />
-        </div>
-        <br />
-        <center>
-          <h4>Didnt Receive Code ? <b><a href="http://localhost:3000/forgot_password">Resend</a></b></h4>
-        </center>
-        <button onClick={updatePassword} style={styles.button}>Update Password</button>
-        <br />
-        <br />
-        <center>
-          <h4> Dont have an account ? <Link to="/sign_up">
-                <b>Sign Up</b>
-       </Link>
-</h4>
-        </center>
-        <br />
-        <center>
-          <h3>OR</h3>
-          
-        </center>
-        <br />
-        <center>
-          <h4> <Link to="/login">
-                <b>Sign In</b>
-       </Link>
-</h4>
-        </center>
-      </div>
-
-      <div id="successMessage" className="success-message" style={{ display: showSuccessMessage ? "block" : "none" }}>
-        <p>Password updated successfully! You can now Sign In with your new password.</p>
-        <p> <b><a href="eg.html">SIGN IN</a></b></p>
       </div>
     </div>
   );
@@ -151,6 +150,7 @@ const ForgotPassword = () => {
 
 const styles = {
   container: {
+    background: '#1C2E3E',
     maxWidth: '400px',
     margin: '50px auto',
     backgroundColor: '#f2f2f2',
@@ -175,7 +175,11 @@ const styles = {
     margin: 'auto',
     position: 'center',
   },
+  pageBackground: {
+    backgroundColor: '#2A4454',
+    minHeight: '100vh',
+    padding: '15px'
+  },
 };
-
 
 export default ForgotPassword;
